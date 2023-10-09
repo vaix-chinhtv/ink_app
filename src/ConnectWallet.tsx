27e3c16a415e3b928924cdd3c62de2eb3cb7d1de
tranchinh <chinhlong30032001@gmail.com>
2023-10-09 12:14:44 +0700

    first

--- src/ConnectWallet.tsx
+++ src/ConnectWallet.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import { useWallet, useAllWallets } from 'useink'
-
+import { useWallet, useAllWallets, useContract, useTx, useTransfer } from 'useink'
+import metadata from './contract.json'
 function ConnectWallet() {
     const { account , connect, disconnect } = useWallet()
     const wallets = useAllWallets()
+    const CONTRACT_ADDRESS = "5CpWuLkLBF8Zq2Ve7DTRTTrGgAbojnPALGD7yDzx3iWiXz41";
+  const contract = useContract(CONTRACT_ADDRESS, metadata)
+  const deposit = useTx(contract, 'deposit')
+  const reward = useTx(contract, 'reward')
 
+  const transfer = useTransfer()
+  const receiver = '5EyR7vEk7DtvEWeefGcXXMV6hKwB8Ex5uvjHufm466mbjJkR'
+  const amount = 1_000_000;
+
+  const handleDeposit = async () => {
+    const args:[] = []
+      const res = await deposit.signAndSend(args, {value: amount})
+      console.log({res});
+  }
+
+  const handleReward = async () => {
+    const args = [["5EyR7vEk7DtvEWeefGcXXMV6hKwB8Ex5uvjHufm466mbjJkR"]]
+      const res = await reward.signAndSend(args)
+      console.log({res});
+  }
 
     if ( !account) {
         return (
@@ -30,10 +48,19 @@ function ConnectWallet() {
       return (
         <>
           <p>You are connected as {account?.name || account.address}</p>
-
           <button onClick={disconnect}>
             Disconnect Wallet
           </button>
+
+          <button
+        type="button"
+        disabled={transfer?.isSubmitting}
+        onClick={() => transfer?.signAndSend(receiver, amount)}
+      >
+        Send $$$
+      </button>
+          <button onClick={() => handleDeposit()} >Deposit</button>
+          <button onClick={() => handleReward()} >Reward</button>
         </>
       )
     }
